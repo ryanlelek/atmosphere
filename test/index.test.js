4c9d284c7089a399cc38cf3e8d10962bdf9ed6c9
Ryan Lelek <ryanlelek@gmail.com>
2014-06-08 19:35:29 -0700

    Adding .get() method to fetch environment variables by name

--- test/index.test.js
+++ test/index.test.js
@@ -7,37 +7,62 @@ require('should');
 // Subject
 var atmosphere = require('../lib/index.js');
 
-describe('Library - Atmosphere', function () {
+describe('Atmosphere Initialization', function () {
 
-  it('should return process.env.PORT if defined', function () {
+  it('should throw an error when provided environment is not an object', function () {
 
-    var instance = atmosphere({ PORT : 1234 }, {});
-    instance.port().should.equal(1234);
+    var exception;
 
-  });
+    try {
+      atmosphere('notanobject');
+    } catch (error) {
+      exception = error;
+    }
 
-  it('should return config.server.port if defined', function () {
-
-    var instance = atmosphere({}, { server : { port : 5678 } });
-    instance.port().should.equal(5678);
-
-  });
-
-  it('should return default of 3000 if neither process.env.PORT nor config.server.port are defined', function () {
-
-    var instance = atmosphere({}, {});
-    instance.port().should.equal(3000);
+    exception.should.have.property('name').and.equal('Error');
+    exception.should.have.property('message').and.equal('Fatal: Provided environment is not an object!');
 
   });
 
-  it('should convert a string port into a number', function () {
-
-    var environment = atmosphere({ PORT : '1234' }, {});
-    environment.port().should.equal(1234);
-
-    var config = atmosphere({}, { server : { port : '5678' } });
-    config.port().should.equal(5678);
-
+  it('should continue when provided environment is an object', function () {
+    var atmo = atmosphere({});
+    atmo.should.be.type('object');
   });
+
+});
+
+describe('Atmosphere.get()', function () {
+
+  it('should return the environment variable when name is given', function () {
+    var atmo = atmosphere({ port : 1234 });
+    atmo.get('port').should.equal(1234);
+  });
+
+  it('should return undefined when the requested environment variable is not set', function () {
+    var atmo = atmosphere({ key : 'value' });
+    (atmo.get('port') === undefined).should.equal(true);
+  });
+
+  it('should return the environment value when a configuration object is given and does not have the same key', function () {
+    var atmo = atmosphere({ port : 1234 }, { key : 'value' });
+    atmo.get('port').should.equal(1234);
+  });
+
+  it('should return the configuration value when a configuration object is given and has the same key', function () {
+    var atmo = atmosphere({ port : 1234 }, { port : 5678 });
+    atmo.get('port').should.equal(5678);
+  });
+
+  it('should automatically uppercase the requested key');
+
+  it('should automatically replace spaces in the requested key');
+
+  it('should support an environment variable prefix');
+
+  it('should convert the return value type to an integer when requested');
+
+  it('should convert the return value type to an string when requested');
+
+  it('should return an intelligent global default (based on best practices) when neither the environment nor configuration match the key');
 
 });
